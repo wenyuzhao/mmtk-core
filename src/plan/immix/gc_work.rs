67eda46809ff33424cd6fb81f6a7658fcb790842
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-09-18 10:04:06 +1000

    cleanup

--- src/plan/immix/gc_work.rs
+++ src/plan/immix/gc_work.rs
@@ -7,7 +7,6 @@ use crate::policy::space::Space;
 use crate::scheduler::gc_work::*;
 use crate::scheduler::{GCWorkerLocal, WorkBucketStage};
 use crate::util::alloc::{Allocator, ImmixAllocator};
-use crate::util::metadata::side_metadata::address_to_meta_address;
 use crate::util::metadata::store_metadata;
 use crate::util::object_forwarding;
 use crate::util::{Address, ObjectReference};
