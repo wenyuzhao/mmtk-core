26b85119232ce724ea2445e2e00aa024477cda1c
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-08 15:17:38 +1100

    Fix lock-free block allocation

--- src/policy/immix/block_allocation.rs
+++ src/policy/immix/block_allocation.rs
@@ -7,14 +7,12 @@ use crate::{
     MMTK,
 };
 use atomic::Ordering;
-use spin::Mutex;
+use spin::RwLock;
 use std::sync::atomic::AtomicUsize;
 
 pub struct BlockAllocation<VM: VMBinding> {
     space: Option<&'static ImmixSpace<VM>>,
-    clean_block_cursor: AtomicUsize,
-    clean_block_buffer: Vec<Block>,
-    clean_block_buffer_refill_lock: Mutex<()>,
+    clean_block_buffer: RwLock<(AtomicUsize, Vec<Block>)>,
     refill_count: usize,
 }
 
@@ -22,15 +20,17 @@ impl<VM: VMBinding> BlockAllocation<VM> {
     pub fn new() -> Self {
         Self {
             space: None,
-            clean_block_cursor: AtomicUsize::new(0),
-            clean_block_buffer: vec![],
-            clean_block_buffer_refill_lock: Mutex::new(()),
+            clean_block_buffer: Default::default(),
             refill_count: num_cpus::get(),
         }
     }
 
     pub fn nursery_blocks(&self) -> usize {
-        self.clean_block_cursor.load(Ordering::Relaxed)
+        unsafe {
+            (*self.clean_block_buffer.as_mut_ptr())
+                .0
+                .load(Ordering::SeqCst)
+        }
     }
 
     pub fn init(&mut self, space: &'static ImmixSpace<VM>) {
@@ -38,15 +38,18 @@ impl<VM: VMBinding> BlockAllocation<VM> {
     }
 
     pub fn reset(&mut self) {
-        self.clean_block_buffer.clear();
-        self.clean_block_cursor.store(0, Ordering::SeqCst);
+        // FIXME: Release blocks after the cursor.
+        let mut buffer = self.clean_block_buffer.write();
+        buffer.0.store(0, Ordering::SeqCst);
+        buffer.1.clear();
     }
 
     pub fn reset_and_generate_nursery_sweep_tasks(
         &mut self,
         num_workers: usize,
     ) -> Vec<Box<dyn GCWork<VM>>> {
-        let blocks = self.clean_block_buffer.len();
+        let mut buffer = self.clean_block_buffer.write();
+        let blocks = buffer.1.len();
         let num_bins = num_workers << 1;
         let bin_cap = blocks / num_bins + if blocks % num_bins == 0 { 0 } else { 1 };
         let mut bins = (0..num_bins)
@@ -54,12 +57,13 @@ impl<VM: VMBinding> BlockAllocation<VM> {
             .collect::<Vec<Vec<Block>>>();
         for i in 0..num_bins {
             for j in 0..bin_cap {
-                if let Some(block) = self.clean_block_buffer.get(i * bin_cap + j) {
+                if let Some(block) = buffer.1.get(i * bin_cap + j) {
                     bins[i].push(*block);
                 }
             }
         }
-        self.reset();
+        buffer.0.store(0, Ordering::SeqCst);
+        buffer.1.clear();
         let space = self.space();
         bins.into_iter()
             .map::<Box<dyn GCWork<VM>>, _>(|blocks| box RCSweepNurseryBlocks { space, blocks })
@@ -101,35 +105,43 @@ impl<VM: VMBinding> BlockAllocation<VM> {
     }
 
     #[inline(always)]
-    fn alloc_clean_block_fast(&self) -> Option<Block> {
-        let buffer: &[Block] = &self.clean_block_buffer;
-        let i = self
-            .clean_block_cursor
+    fn alloc_clean_block_fast_impl(&self, buffer: &(AtomicUsize, Vec<Block>)) -> Option<Block> {
+        let len = buffer.1.len();
+        let i = buffer
+            .0
             .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |i| {
-                if i >= buffer.len() {
+                if i >= len {
                     None
                 } else {
                     Some(i + 1)
                 }
             })
             .ok()?;
-        Some(buffer[i])
+        Some(buffer.1[i])
+    }
+
+    #[inline(always)]
+    fn alloc_clean_block_fast(&self) -> Option<Block> {
+        let buffer = self.clean_block_buffer.read();
+        self.alloc_clean_block_fast_impl(&buffer)
     }
 
     #[cold]
     fn alloc_clean_block_slow(&mut self, tls: VMThread) -> Option<Block> {
-        let _guard = self.clean_block_buffer_refill_lock.lock();
+        let mut buffer = self.clean_block_buffer.write();
         // Retry allocation
-        if let Some(block) = self.alloc_clean_block_fast() {
+        if let Some(block) = self.alloc_clean_block_fast_impl(&buffer) {
             return Some(block);
         }
         // Fill buffer with N blocks
+        let r = Block::from(self.space().acquire_uninterruptable(tls, Block::PAGES)?);
+        buffer.1.push(r);
         for _ in 0..self.refill_count {
             let a = self.space().acquire_uninterruptable(tls, Block::PAGES)?;
-            self.clean_block_buffer.push(Block::from(a));
+            buffer.1.push(Block::from(a));
         }
-        let r = self.space().acquire_uninterruptable(tls, Block::PAGES)?;
-        Some(Block::from(r))
+        buffer.0.fetch_add(1, Ordering::SeqCst);
+        Some(r)
     }
 
     /// Allocate a clean block.
