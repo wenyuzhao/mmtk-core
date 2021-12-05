4a13271a473ddb248f51ec59de11d1a0384bd7c7
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-12-06 01:23:08 +1100

    Remove unnecessary mark

--- src/policy/immix/chunk.rs
+++ src/policy/immix/chunk.rs
@@ -401,7 +401,7 @@ impl<VM: VMBinding> SweepDeadCyclesChunk<VM> {
         if !crate::args::HOLE_COUNTING {
             Block::inc_dead_bytes_sloppy_for_object::<VM>(o);
         }
-        self.immix().mark(o);
+        // self.immix().mark(o);
         rc::set(o, 0);
         if !crate::args::BLOCK_ONLY {
             rc::unmark_straddle_object::<VM>(o)
