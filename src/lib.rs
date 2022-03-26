9ccef4c632c4692afdf86fd141cec506f7b26a31
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2022-03-26 21:35:39 +1100

    counters

--- src/lib.rs
+++ src/lib.rs
@@ -291,6 +291,9 @@ struct Pauses {
     pub min_used_pages: AtomicUsize,
     pub max_used_pages: AtomicUsize,
     pub gc_with_unfinished_lazy_jobs: AtomicUsize,
+    pub incs_triggerd: AtomicUsize,
+    pub alloc_triggerd: AtomicUsize,
+    pub overflow_triggerd: AtomicUsize,
 }
 
 impl Pauses {
@@ -312,6 +315,9 @@ impl Pauses {
         print!("total_used_pages\t");
         print!("min_used_pages\t");
         print!("max_used_pages\t");
+        print!("incs_triggerd\t");
+        print!("alloc_triggerd\t");
+        print!("overflow_triggerd\t");
     }
     pub fn print_values(&self) {
         print!("{}\t", self.rc.load(Ordering::SeqCst));
@@ -343,6 +349,9 @@ impl Pauses {
         print!("{}\t", self.total_used_pages.load(Ordering::SeqCst));
         print!("{}\t", self.min_used_pages.load(Ordering::SeqCst));
         print!("{}\t", self.max_used_pages.load(Ordering::SeqCst));
+        print!("{}\t", self.incs_triggerd.load(Ordering::SeqCst));
+        print!("{}\t", self.alloc_triggerd.load(Ordering::SeqCst));
+        print!("{}\t", self.overflow_triggerd.load(Ordering::SeqCst));
     }
 }
 
@@ -360,6 +369,9 @@ static PAUSES: Pauses = Pauses {
     min_used_pages: AtomicUsize::new(usize::MAX),
     max_used_pages: AtomicUsize::new(0),
     gc_with_unfinished_lazy_jobs: AtomicUsize::new(0),
+    incs_triggerd: AtomicUsize::new(0),
+    alloc_triggerd: AtomicUsize::new(0),
+    overflow_triggerd: AtomicUsize::new(0),
 };
 
 #[derive(Default)]
