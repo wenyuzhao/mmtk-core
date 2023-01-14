7cff1db1b32634eb63033076c44e2012b1c8a4a6
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-01-14 22:47:57 +0000

    WIP: Temporarily disable lazy young block sweeping

--- src/policy/immix/block_allocation.rs
+++ src/policy/immix/block_allocation.rs
@@ -56,7 +56,7 @@ impl<VM: VMBinding> BlockAllocation<VM> {
 
     /// Reset allocated_block_buffer and free nursery blocks.
     pub fn sweep_and_reset(&mut self, scheduler: &GCWorkScheduler<VM>) {
-        const MAX_STW_SWEEP_BLOCKS: usize = 1024;
+        const MAX_STW_SWEEP_BLOCKS: usize = usize::MAX;
         let _guard = self.refill_lock.lock().unwrap();
         let space = self.space();
         // Sweep nursery blocks
