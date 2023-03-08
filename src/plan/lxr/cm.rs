49033052f21880bcf9114b472b8f3b1099a48929
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-03-08 12:43:00 -0800

    minor fix

--- src/plan/lxr/cm.rs
+++ src/plan/lxr/cm.rs
@@ -455,7 +455,7 @@ impl<VM: VMBinding, const COMPRESSED: bool> LXRStopTheWorldProcessEdges<VM, COMP
         let object = slot.load::<COMPRESSED2>();
         let new_object = self.trace_and_mark_object(object);
         super::record_edge_for_validation(slot, new_object);
-        if Self::OVERWRITE_REFERENCE && !new_object.is_null() {
+        if Self::OVERWRITE_REFERENCE && new_object != object && !new_object.is_null() {
             slot.store::<COMPRESSED2>(new_object);
         }
     }
