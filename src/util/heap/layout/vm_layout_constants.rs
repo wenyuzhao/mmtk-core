8e0c9953ef3c736466b677f2a1d89b872763dec6
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-04-19 11:34:13 -0700

    Remove `virt_constraint`

--- src/util/heap/layout/vm_layout_constants.rs
+++ src/util/heap/layout/vm_layout_constants.rs
@@ -119,19 +119,10 @@ impl VMLayoutConstants {
                 .unwrap();
         }
 
-        let heap_end = if cfg!(feature = "virt_constraint") {
-            usize::min(
-                end,
-                (start + heap_size - 1 + BYTES_IN_CHUNK) & !(BYTES_IN_CHUNK - 1),
-            )
-        } else {
-            end
-        };
-
         Self {
             log_address_space: 35,
             heap_start: chunk_align_down(unsafe { Address::from_usize(start) }),
-            heap_end: chunk_align_up(unsafe { Address::from_usize(heap_end) }),
+            heap_end: chunk_align_up(unsafe { Address::from_usize(end) }),
             vm_space_size: chunk_align_up(unsafe { Address::from_usize(0x800_0000) }).as_usize(),
             max_chunks: (end - start) >> LOG_BYTES_IN_CHUNK,
             log_space_extent: 31,
