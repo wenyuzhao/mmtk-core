ccaa72d91e193797fc8752bd208040d469573c49
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-06-06 12:37:46 -0700

    Update SATB trigger constants

--- src/plan/lxr/global.rs
+++ src/plan/lxr/global.rs
@@ -290,8 +290,9 @@ impl<VM: VMBinding> Plan for LXR<VM> {
             let d = SystemTime::now().duration_since(date230505).unwrap();
             let hrs = (d.as_secs() / 3600) % 24;
             let new_value: usize = match hrs {
-                _ if hrs < 12 => 128,
-                _ => 256,
+                _ if hrs < 8 => 256,
+                _ if hrs < 16 => 192,
+                _ => 128,
             };
             if new_value != MAX_RC_PAUSES_BEFORE_SATB.load(Ordering::Relaxed) {
                 gc_log!([1] "===>>> Update SATB Trigger: {:?} <<<===", new_value);
