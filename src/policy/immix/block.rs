9976778df690e008b341ded450aded3ba0644f9e
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-06-13 12:20:11 -0700

    Revert "Reduce CAS"
    This reverts commit bb2dc88b593421e63bb076bc2d21204d22abf822.

--- src/policy/immix/block.rs
+++ src/policy/immix/block.rs
@@ -471,15 +471,15 @@ impl Block {
     }
 
     pub fn set_as_in_place_promoted(&self) {
-        unsafe { Self::NURSERY_PROMOTION_STATE_TABLE.store(self.start(), 1u8) };
+        Self::NURSERY_PROMOTION_STATE_TABLE.fetch_or_atomic(self.start(), 1u8, Ordering::Relaxed);
     }
 
     pub fn is_in_place_promoted(&self) -> bool {
-        unsafe { Self::NURSERY_PROMOTION_STATE_TABLE.load::<u8>(self.start()) != 0 }
+        Self::NURSERY_PROMOTION_STATE_TABLE.load_atomic::<u8>(self.start(), Ordering::Relaxed) != 0
     }
 
     pub fn clear_in_place_promoted(&self) {
-        unsafe { Self::NURSERY_PROMOTION_STATE_TABLE.store(self.start(), 0u8) };
+        Self::NURSERY_PROMOTION_STATE_TABLE.store_atomic(self.start(), 0u8, Ordering::Relaxed)
     }
 
     pub fn unlog(&self) {
