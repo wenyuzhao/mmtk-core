d23e8f43867aeb75c9e6f5ba8a545e8960a594e1
Wenyu Zhao <wenyuzhaox@gmail.com>
2024-08-15 15:04:48 +1000

    utilization

--- src/scheduler/worker.rs
+++ src/scheduler/worker.rs
@@ -13,6 +13,7 @@ use crossbeam::queue::ArrayQueue;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 /// Represents the ID of a GC worker thread.
 pub type ThreadId = usize;
@@ -288,7 +289,10 @@ impl<VM: VMBinding> GCWorker<VM> {
 
             #[cfg(feature = "tracing")]
             probe!(mmtk, work, typename.as_ptr(), typename.len());
+            let t = Instant::now();
             work.do_work_with_stat(&mut self, mmtk);
+            let us = t.elapsed().as_micros();
+            super::TOTAL_BUSY_TIME_US.fetch_add(us as usize, Ordering::SeqCst);
             std::mem::drop(work);
             flush_logs!();
         }
